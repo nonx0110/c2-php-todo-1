ee8c179e3a142895e55e4e57c30d3b6b541247f4
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-11-25 12:22:22 +0900

    ステータス表示途中

--- app/Todo.php
+++ app/Todo.php
@@ -7,5 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     const STATUS_NOT_YET = 0;
+    const STATUS = ['未着手','作業中','終了'];
     protected $fillable = ['title', 'due_date', 'status'];
+
+    /**
+ * 
+ * @return string
+ */
+    public function getStatusText(): date_interval_create_from_date_string
+    {
+        if(empty(self::STATUS[$this->status]))
+        {
+            return "";
+        }
+
+        return self::STATUS[$this->status];
+    }
 }
+
